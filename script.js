cae21a1b24823182c21c2a95dff4737bfd51dce8
twf3238 <twf3238@aol.com>
2024-12-09 08:13:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const reviewBtn = document.getElementById('reviewBtn');
 
     // Airtable API Base ID and Table Name
-    const AIRTABLE_BASE_ID = 'Revision Langues'; // Remplacez par votre Base ID
+    const AIRTABLE_BASE_ID = 'appu2zFKRQzZkCUkC'; // Remplacez par votre Base ID
     const AIRTABLE_TABLE_NAME = 'Exercices'; // Nom de votre table
     const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
 
