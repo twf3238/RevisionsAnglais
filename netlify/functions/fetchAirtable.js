e0ff56296633dcd3cc9121222cdc78f00019c933
twf3238 <twf3238@aol.com>
2024-12-12 21:42:37 +0100

    Add files via upload

--- netlify/functions/fetchAirtable.js
+++ netlify/functions/fetchAirtable.js
@@ -4,7 +4,7 @@ exports.handler = async () => {
     const AIRTABLE_BASE_ID = 'appu2zFKRQzZkCUkC'; // Remplacez par votre Base ID
     const AIRTABLE_TABLE_NAME = 'Exercices'; // Nom de votre table
     const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}`;
-    const AIRTABLE_ACCESS_TOKEN = process.env.AIRTABLE_ACCESS_TOKEN; //patTlNKpueJ6jGFgx.55d7a901c50a640cbfb1451f9594531f9065d56620f4d4fadfc5601e316942aa
+    const AIRTABLE_ACCESS_TOKEN = patTlNKpueJ6jGFgx.55d7a901c50a640cbfb1451f9594531f9065d56620f4d4fadfc5601e316942aa // process.env.AIRTABLE_ACCESS_TOKEN; //patTlNKpueJ6jGFgx.55d7a901c50a640cbfb1451f9594531f9065d56620f4d4fadfc5601e316942aa
 
     try {
         const response = await fetch(AIRTABLE_URL, {
